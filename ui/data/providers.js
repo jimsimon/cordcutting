3a8867b316c1c2ef5326fbb02a52dea0e198fd5d
Jim Simon <jim.j.simon@gmail.com>
2017-05-28 21:31:20 -0400

    Move ui code to subfolder and add initial api code

---
+++ ui/data/providers.js
@@ -0,0 +1,56 @@
+window.bundles = [
+  {
+    name: "DirectTV Now",
+    channels: [
+
+    ]
+  },
+  {
+    name: "Hulu Live",
+    channels: [
+
+    ]
+  },
+  {
+    name: "PlayStation Vue (Slim)",
+    channels: [
+
+    ]
+  },
+  {
+    name: "PlayStation Vue ()",
+    channels: [
+
+    ]
+  },
+  {
+    name: "PlayStation Vue ()",
+    channels: [
+
+    ]
+  },
+  {
+    name: "PlayStation Vue ()",
+    channels: [
+
+    ]
+  },
+  {
+    name: "Sling TV (Blue)",
+    channels: [
+
+    ]
+  },
+  {
+    name: "Sling TV (Orange)",
+    channels: [
+
+    ]
+  },
+  {
+    name: "YouTube TV",
+    channels: [
+
+    ]
+  }
+]
