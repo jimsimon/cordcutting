f9609a3344d30fbd0fffdff813ec16fed4f7865a
Jim Simon <jim.j.simon@gmail.com>
2017-05-31 02:13:01 -0400

    Add built-in hot reloading

--- api/index.js
+++ api/index.js
@@ -1,60 +1,18 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const Umzug = require('umzug')
-const { sequelize } = require('./util/sequelize')
-const { Provider, Bundle, Channel } = require('./models/index')
+const chokidar = require('chokidar');
+const invalidate = require('invalidate-module');
+const path = require('path');
 
-const app = express()
-const umzug = new Umzug({
-  storage: 'sequelize',
-  storageOptions: {
-    sequelize
-  }
-})
+let server = require('./app')
 
-app.use('*', function (req, res, next) {
-  umzug.pending().then(function (migrations) {
-    if (migrations && migrations.length > 0) {
-      res.status(500).send("You have pending database migrations, please run them before continuing.")
-    } else {
-      next()
-    }
-  });
-})
+// Watch for file changes and reload the server
+chokidar.watch('.', {
+  ignoreInitial: true,
+  ignored: [/(^|[\/\\])\../, '**/node_modules/**']
+}).on('all', (event, filepath) => {
+  const absFilename = path.resolve(filepath);
+  invalidate(absFilename);
+  server.close(() => {
+    server = require('./app')
+  })
+});
 
-app.use(bodyParser.json())
-
-app.post('/wizardResult', async function (req, res) {
-  const userSelectedChannels = req.body
-
-  const results = []
-  const bundles = await Bundle.findAll()
-  for (const bundle of bundles) {
-    const result = {
-      provider: (await bundle.getProvider()).name,
-      bundle: bundle.name
-    }
-
-    // TODO: Add unique index for name
-    const channels = await bundle.getChannels({where: {name: {$in: userSelectedChannels}}})
-    const foundChannels = channels.map(c => c.name)
-    result.found = foundChannels
-
-    const userSelectedChannelsSet = new Set(userSelectedChannels)
-    for (const channel of foundChannels) {
-      userSelectedChannelsSet.delete(channel)
-    }
-    result.missing = [...userSelectedChannelsSet]
-    results.push(result)
-  }
-  res.json(results)
-})
-
-app.get('/channels', async function (req, res) {
-  const channels = await Channel.findAll({ order: [['name', 'ASC']] })
-  res.json(channels)
-})
-
-app.listen(3000, function () {
-  console.log('API listening on port 3000!')
-})
