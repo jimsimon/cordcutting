740f231bf60da1823b213ca8765c0ed5d7f08f38
Jim Simon <jim.j.simon@gmail.com>
2017-06-27 01:42:41 -0400

    Refactor router to its own file and use async compatible router impl

--- api/app.js
+++ api/app.js
@@ -2,9 +2,9 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const Umzug = require('umzug')
+const router = require('./router/router')
 const env = process.env.NODE_ENV
 const { sequelize } = require('./util/sequelize')
-const { Bundle, Category, Channel, WizardRequest } = require('./models/index')
 
 const app = express()
 const umzug = new Umzug({
@@ -39,34 +39,7 @@ app.get('/', function (req, res) {
   res.send('API is healthy!')
 })
 
-app.post('/api/wizardResults', async function (req, res) {
-
-  const wizardResult = await buildWizardResultForChannels(req.body);
-  res.json(wizardResult)
-})
-
-app.get('/api/wizardResults', async function (req, res) {
-  const wizardRequestId = req.query.wizardRequestId
-
-  if (!wizardRequestId) {
-    return res.sendStatus(403)
-  }
-
-  const wizardRequest = await WizardRequest.findById(parseInt(wizardRequestId, 10))
-  if (!wizardRequest) {
-    return res.sendStatus(404)
-  }
-
-  const userSelectedChannelIds = await wizardRequest.getChannels({attributes: 'id'})
-  const wizardResult = buildWizardResultForChannels(userSelectedChannelIds)
-
-  res.json(wizardResult)
-})
-
-app.get('/api/categories', async function (req, res) {
-  const categories = await Category.findAll({ order: [['name', 'ASC'], [Channel, 'name', 'ASC']], include: [Channel]})
-  res.json(categories)
-})
+app.use('/api', router)
 
 app.use(function (err, req, res, next) {
   if (res.headersSent) {
@@ -79,42 +52,6 @@ app.use(function (err, req, res, next) {
     <p>${err.stack}</p>
   `)
 })
-
-async function buildWizardResultForChannels(userSelectedChannelIds) {
-  const userSelectedChannels = await Channel.findAll({where: {id: {$in: userSelectedChannelIds}}})
-  const wizardRequest = await WizardRequest.create({
-    channels: userSelectedChannels
-  })
-
-  const userSelectedChannelMap = userSelectedChannels.reduce(function (map, channel) {
-    map.set(channel.id, channel)
-    return map;
-  }, new Map());
-
-  const results = []
-  const bundles = await Bundle.findAll()
-  for (const bundle of bundles) {
-    const result = {
-      provider: (await bundle.getProvider()).name,
-      bundle: bundle.name,
-      totalPrice: bundle.price // TODO: Change this when addons are added in
-    }
-
-    const bundleChannels = await bundle.getChannels({where: {id: {$in: userSelectedChannelIds}}})
-    result.found = bundleChannels
-
-    const missingChannelMap = new Map(userSelectedChannelMap)
-    for (const channel of bundleChannels) {
-      missingChannelMap.delete(channel.id)
-    }
-    result.missing = [...missingChannelMap.values()]
-    results.push(result)
-  }
-  return {
-    wizardRequestId: wizardRequest.id,
-    results
-  };
-}
 
 const server = app.listen(3000, function () {
   console.log('API listening on port 3000!')
---
+++ api/router/router.js
@@ -0,0 +1,69 @@
+const { Bundle, Category, Channel, WizardRequest } = require('../models/index')
+const router = require('express-promise-router')()
+
+router.post('/api/wizardResults', async function (req, res) {
+
+  const wizardResult = await buildWizardResultForChannels(req.body);
+  res.json(wizardResult)
+})
+
+router.get('/api/wizardResults', async function (req, res) {
+  const wizardRequestId = req.query.wizardRequestId
+
+  if (!wizardRequestId) {
+    return res.sendStatus(403)
+  }
+
+  const wizardRequest = await WizardRequest.findById(parseInt(wizardRequestId, 10))
+  if (!wizardRequest) {
+    return res.sendStatus(404)
+  }
+
+  const userSelectedChannelIds = await wizardRequest.getChannels({attributes: 'id'})
+  const wizardResult = buildWizardResultForChannels(userSelectedChannelIds)
+
+  res.json(wizardResult)
+})
+
+router.get('/api/categories', async function (req, res) {
+  const categories = await Category.findAll({ order: [['name', 'ASC'], [Channel, 'name', 'ASC']], include: [Channel]})
+  res.json(categories)
+})
+
+async function buildWizardResultForChannels(userSelectedChannelIds) {
+  const userSelectedChannels = await Channel.findAll({where: {id: {$in: userSelectedChannelIds}}})
+  const wizardRequest = await WizardRequest.create({
+    channels: userSelectedChannels
+  })
+
+  const userSelectedChannelMap = userSelectedChannels.reduce(function (map, channel) {
+    map.set(channel.id, channel)
+    return map;
+  }, new Map());
+
+  const results = []
+  const bundles = await Bundle.findAll()
+  for (const bundle of bundles) {
+    const result = {
+      provider: (await bundle.getProvider()).name,
+      bundle: bundle.name,
+      totalPrice: bundle.price // TODO: Change this when addons are added in
+    }
+
+    const bundleChannels = await bundle.getChannels({where: {id: {$in: userSelectedChannelIds}}})
+    result.found = bundleChannels
+
+    const missingChannelMap = new Map(userSelectedChannelMap)
+    for (const channel of bundleChannels) {
+      missingChannelMap.delete(channel.id)
+    }
+    result.missing = [...missingChannelMap.values()]
+    results.push(result)
+  }
+  return {
+    wizardRequestId: wizardRequest.id,
+    results
+  };
+}
+
+module.exports = router
