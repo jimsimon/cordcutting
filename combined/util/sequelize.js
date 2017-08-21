2e643c1c86208ed9698e59bd76c15f84c553912e
Jim Simon <jim.j.simon@gmail.com>
2017-08-21 01:28:33 -0400

    Initial work on combining projects

--- combined/util/sequelize.js
+++ combined/util/sequelize.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize')
-const env = process.env.NODE_ENV
+const env = process.env.NODE_ENV || 'development'
 const config = require('../config/config')[env]
 
 let sequelize
